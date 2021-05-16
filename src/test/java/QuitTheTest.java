5c8ffa7dab7048544270aa1146a12829038f72ae
Ashawami Laxman Mulik <ashawami@Ashawamis-MacBook-Pro.local>
2021-05-16 08:03:09 +0530

    Initial commit

---
+++ src/test/java/QuitTheTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class QuitTheTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void initialSetup(){
+        System.setProperty("webdriver.chrome.driver" , "/Users/ashawami/Downloads/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://in.indeed.com/");
+    }
+
+    @AfterMethod
+    public void endSetup(){
+        driver.quit();
+    }
+
+    @Test
+    public void testDriverDotQuit() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement login_button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("gnav-LoggedOutAccountLink")));
+        login_button.click();
+        WebElement facebook_login = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pass-SocialButton--facebook")));
+        facebook_login.click();
+    }
+}
